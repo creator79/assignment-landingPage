0774a90442915c2db3e2b45071c23d2cdb8afa5a
mahi-19 <upadhyayvivek.19@gmail.com>
2024-01-13 20:09:16 +0530

    fix: frontend

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost', 'res.cloudinary.com', 'images.unsplash.com', 'images.pexels.com', 'images.pixabay.com', 'cdn.pixabay.com' ,'media.istockphoto.com'],
+        domains: ['localhost', 'res.cloudinary.com', 'images.unsplash.com', 'images.pexels.com', 'images.pixabay.com', 'cdn.pixabay.com' ,'media.istockphoto.com','www.freepik.com'],
     },
 }
 
